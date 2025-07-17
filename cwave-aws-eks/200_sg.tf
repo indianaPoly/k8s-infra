c2052240d666d3f6e5d6f87c7a2aa6bb9d3b5602
indianapoly <hyeonlimgo5@gmail.com>
2025-07-17 14:28:35 +0900

    add: 200, 600 terraform

---
+++ cwave-aws-eks/200_sg.tf
@@ -0,0 +1,27 @@
+# 보안 그룹 설정: SSH(22) 및 HTTP(80) 트래픽 허용
+resource "aws_security_group" "nginx_sg" {
+    name_prefix = "nginx-sg"
+
+    ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+}
